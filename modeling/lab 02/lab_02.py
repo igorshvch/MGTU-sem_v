e86443c2f6a21374de823419c295152856a0f68f
igorshvch <igorshvch@gmail.com>
2021-12-28 01:25:46 +0300

    modeling/lab 02/feat: add SLE solving func

--- modeling/lab 02/lab_02.py
+++ modeling/lab 02/lab_02.py
@@ -1,184 +1,64 @@
-__version__ = "0.0.1"
+import math
 
-arr_x = [0, 0.15, 0.3, 0.45, 0.6, 0.75, 0.9, 1.05]
-arr_y = [1, 0.838771, 0.655336, 0.450447, 0.225336, -0.01831, -0.27839, -0.55243]
-
-def sort_coord_table(arr_x, arr_y, print_info=True):
-    table = [(x, y) for (x, y) in zip(arr_x, arr_y)]
-    table = sorted(table, key=lambda pair: pair[0])
-
-    arr_x_sorted = [pair[0] for pair in table]
-    arr_y_sorted = [pair[1] for pair in table]
-
-    #секция печати
-    if print_info:
-        print()
-        print("Отсортированная таблица координат")
-        print("X:", end=" ")
-        [print("{:9.6f}".format(i), end=" ") for i in arr_x_sorted]
-        print()
-        print("Y:", end=" ")
-        [print("{:9.6f}".format(i), end=" ") for i in arr_y_sorted]
-        print()
-    #
-
-    return arr_x_sorted, arr_y_sorted
-
-def binary_search(arr, x):
+def machine_epsilon(func=float):
     '''
-    Функция бинарного поиска по сортированному массиву
+    Вспомогательная функция для вычисления машинного нуля
     '''
-    lo = 0
-    hi = len(arr)
-    while lo < hi:
-        mid = (lo+hi)//2
-        if x < arr[mid]:
-            hi = mid
-        else:
-            lo = mid+1
-    return lo if lo>0 else 0
+    machine_epsilon = func(1)
+    while func(1)+func(machine_epsilon) != func(1):
+        machine_epsilon_last = machine_epsilon
+        machine_epsilon = func(machine_epsilon) / func(2)
+    return machine_epsilon_last
 
-def get_nodes(arr_x, arr_y, start_pos, n, print_info=True):
+def eval_Lm(x, n):
     '''
-    Функция поиска подходящих узлов
+    Вычисление полинома Лежандра n-ой степени
     '''
-    half_range = (n+1)//2
-    start_pos = start_pos - half_range
-    end_pos = start_pos + n+1
-    left_border = start_pos if start_pos > 0 else 0
-    right_border = end_pos if end_pos < len(arr_x) else len(arr_x)
-
-    if left_border!=right_border:
-        arr_x_nodes = arr_x[left_border:right_border]
-        arr_y_nodes = arr_y[left_border:right_border]
+    res = None
+    if n > 1:
+        Lm0 = 1 #значение полинома 0-й степени
+        Lm1 = x #значение полинома 1-й степени
+        lim_n = 2
+        while True:
+            res = (
+                ((2*lim_n-1)*x*Lm1 - (lim_n-1)*Lm0)
+                /lim_n
+            )
+            Lm0 = Lm1
+            Lm1 = res
+            if lim_n == n:
+                break
+            lim_n += 1
+        return res
     else:
-        arr_x_nodes = [arr_x[left_border]]
-        arr_y_nodes = [arr_y[left_border]]
+        return 1 if pow == 0 else x
 
-    
-    #Секция печати
-    if print_info:
-        print()
-        print("Индекс ближайшего узла в массиве:", start_pos)
-        print("Индексы границ:",[left_border, right_border])
-        print()
-        print("Узловые значения, используемые для вычисления:")
-        print("X:", end=" ")
-        [print("{:9.6f}".format(i), end=" ") for i in arr_x_nodes]
-        print()
-        print("Y:", end=" ")
-        [print("{:9.6f}".format(i), end=" ") for i in arr_y_nodes]
-        print()
-        print()
-    #
-
-    return arr_x_nodes, arr_y_nodes
-
-def calc_dd_coefs(arr_x, arr_y, print_info=True):
+def eval_Lm_derivative(x, n):
     '''
-    Функция вычисления значения разделенных разностей
+    Вычисление производной полинома Лежандра
     '''
-    n = len(arr_y)
-    coefs_table = [[0 for i in range(n)] for i in range(n)]
-    # the first column is y
-    for i in range(n):
-        coefs_table[i][0] = arr_y[i]
-    
-    for j in range(1,n):
-        for i in range(n-j):
-            coefs_table[i][j] = \
-                (coefs_table[i+1][j-1] - coefs_table[i][j-1]) / (arr_x[i+j]-arr_x[i])
-    
-    #Секция печати
-    if print_info:
-        print("Таблица коэффициентов (разделенные разности)")
-        for i in range(len(coefs_table)):
-            for j in range(len(coefs_table[0])):
-                print("{:9.6f}".format(coefs_table[i][j]), end=" ")
-            print()
-        print()
-    #
+    L_first = eval_Lm(x, n-1)
+    L_second = eval_Lm(x, n)
+    return (n/(1-x**2))*(L_first - x*L_second)
 
-    return [coefs_table[0][i] for i in range(len(coefs_table))]
-
-def calc_newton_polynomial(coefs_vector, arr_x, x_dot):
+def eval_Lm_roots(n):
     '''
-    Функиця вычисления значения полинома для заданной точки на оси Х
+    Вычисление заданного количества корней полинома Лежандра
     '''
-    n = len(arr_x)-1
-    p = coefs_vector[n]
+    ME = machine_epsilon()*10
+    roots_res = []
+    roots_init_approx = [
+        math.cos(
+            (math.pi*(4*i-1)) / (4*n+2)
+        )
+        for i in range(1, n+1)
+    ]
+    for x in roots_init_approx:
+        Lm = 1
+        while Lm > ME:
+            Lm = eval_Lm(x, n)
+            Lm_deriv = eval_Lm_derivative(x, n)
+            x = x - Lm/Lm_deriv
+        roots_res.append(x)
+    return roots_res
 
-    for k in range(1,n+1):
-        p = coefs_vector[n-k] + (x_dot - arr_x[n-k])*p
-
-    return p
-
-def main(x_dot, n, print_info=True):
-    
-    #Секция печати
-    if print_info:
-        print()
-        print("Таблица значений функции")
-        print("X:", end=" ")
-        [print("{:9.6f}".format(i), end=" ") for i in arr_x]
-        print()
-        print("Y:", end=" ")
-        [print("{:9.6f}".format(i), end=" ") for i in arr_y]
-        print()
-    #
-    
-    #Вычисление полинома заданной функции
-    arr_x_sorted, arr_y_sorted = sort_coord_table(arr_x, arr_y, print_info=print_info)
-    start_pos = binary_search(arr_x_sorted, x_dot)
-    arr_x_new, arr_y_new = get_nodes(arr_x_sorted, arr_y_sorted, start_pos, n, print_info=print_info)
-    coefs_vector = calc_dd_coefs(arr_x_new, arr_y_new, print_info=print_info)
-    y_dot = calc_newton_polynomial(coefs_vector, arr_x_new, x_dot)
-
-    #Секция печти
-    if print_info:
-        print("========================")
-        print("Вычисление корня функции")
-        print("========================")
-
-    #Вычисление значения корня функции
-    arr_x_sorted, arr_y_sorted = sort_coord_table(arr_y, arr_x, print_info=print_info)
-    start_pos = binary_search(arr_x_sorted, 0)
-    arr_x_new, arr_y_new = get_nodes(arr_x_sorted, arr_y_sorted, start_pos, n, print_info=print_info)
-    coefs_vector = calc_dd_coefs(arr_x_new, arr_y_new, print_info=print_info)
-    func_root = calc_newton_polynomial(coefs_vector, arr_x_new, 0)
-
-    return y_dot, func_root
-
-if __name__ == "__main__":
-    print()
-    while True:
-        x_dot = float(input("Введите точку на оси X в интервале [0, 1.05]:/>>> "))
-        if x_dot > arr_x[-1] or x_dot < arr_x[0]:
-            print("Введенное значение находится за пределами заданного интервала")
-            stop = input("Продолжить [y/n]?/>>> ")
-            if stop == "n":
-                break
-            else:
-                continue
-        n = int(input("Введите степень полинома в интервале [0, 4]:/>>> "))
-        if n > 4 or n < 0:
-            print("Введенное значение находится за пределами заданного интервала")
-            stop = input("Продолжить [y/n]?/>>> ")
-            if stop == "n":
-                break
-            else:
-                continue
-        should_print = input("Печатать подробный результат [y/n]?/>>> ")
-        if should_print == "y":
-            print_info = True
-        else:
-            print_info = False
-        print()
-        if print_info:
-            print("========================================================================")
-        y_dot_poly, func_root = main(x_dot, n, print_info=print_info)
-        print("========================================================================")
-        print("Значение полинома {}й степени в точке {} на оси Х:".format(n, x_dot), y_dot_poly)
-        print("Значение корня функции, полученное путем обратной интерполяции:", func_root)
-        print("========================================================================")
-        print()
